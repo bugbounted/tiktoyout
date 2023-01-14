8db1834c59756de8f9ef89077aa123c0bf85bc0a
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-14 15:26:26 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,58 +1,45 @@
- #Python Bot Source
+import requests
+from bs4 import BeautifulSoup
 import os
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
- 
-# Set up Chrome options to run headless and disable GPU
-chrome_options = Options()  
-chrome_options.add_argument("--headless")  
-chrome_options.add_argument("--disable-gpu")  
- 
-# Create a new instance of the Chrome driver with the options set above 
-driver = webdriver.Chrome(options=chrome_options)  
+import shutil
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.chrome.options import Options 
 
- # Scrape 10 videos from TikTok 
-for i in range(10): 
 
-    # Open TikTok website in headless browser 
-    driver.get('https://www.tiktok.com/')
+#Scraping 10 videos from TikTok using requests and BeautifulSoup library 
+url = 'https://www.tiktok.com/' 
+page = requests.get(url) 
+soup = BeautifulSoup(page.content, 'html5lib') 
+videos = soup.find_all('video', class_='jsx-3523532890 video-player') 
 
-    # Find and click on the video element on the page 
-    video = driver.find_element_by_class_name('jsx-3523532890')  
+ #Downloading the videos from TikTok using shutil library  
+for i in range(10):  
+    video_url = videos[i]['src']  
 
-    # Download the video file to local directory 
-    video.click()  
+    # Download the video  
+    r = requests.get(video_url, stream=True)  
 
-    time.sleep(2)  
+    # Download started  
+    with open('video'+str(i)+'.mp4', 'wb') as f:  
 
- # Concatenate all videos into one file named final.mp4 
-os.system('ffmpeg -f concat -safe 0 -i mylist.txt -c copy final.mp4')  
+        shutil.copyfileobj(r.raw, f)  
 
- # Upload final video to YouTube using headless Selenium browser 
+    # Download completed  
 
- # Open YouTube website in headless browser 
-driver = webdriver.Chrome(options=chrome_options)  
+    print('Downloaded ' + str(i+1) + ' video!')  
 
- # Log into YouTube account using credentials stored in environment variables 
-username = os.environ['YOUTUBE_USERNAME']  
-password = os.environ['YOUTUBE_PASSWORD']  
+     #Concatenating all the downloaded videos into one file using os library    
+os.system("ffmpeg -f concat -safe 0 -i mylist.txt -c copy final_video.mp4") 
 
- # Find and fill out username field on login page with stored credentials 																     
- driver .findElement(By .name("username")) .sendKeys(username);    
+ #Renaming the concatenated file to final.mp4 using os library    
+os.rename("final_video.mp4", "final.mp4") 
 
- # Find and fill out password field on login page with stored credentials                                                                       
- driver .findElement(By .name("password")) .sendKeys(password);    
-
- # Click submit button to log into account                                                                                                    
- driver .findElement(By .id("submit-button")) .click();    
-
- # Navigate to upload page on YouTube website                                                                                                  
- driver .get('https://www.youtube/upload');    
-
- # Find and upload final video file from local directory to YouTube account using Selenium WebDriver API methods                         
- driver .findElement(By .id("upload-prompt-box")) .sendKeys("finalvideofilepath");    
-
- # Set up loop to repeat process hourly (3600 seconds) while True:      
- os .system('ffmpeg -f concat -safe 0 -i mylisttxt -c copy finalmp4')      driver = webdriverChrome (options=chromeOptions)      username = os environ['YOUTUBEUSERNAME']      password = os environ['YOUTUBEPASSWORD']      driver findElement (By name("username")) sendKeys (username);      driver findElement (By name("password")) sendKeys (password);      driver findElement (By id("submitbutton")) click();      driver get('https://wwwyoutubecom/upload');      driver findElement (By id("uploadpromptbox")) sendKeys ("finalvideofilepath");       time sleep (3600)
+ #Auto Uploading the concatenated file to YouTube Channel using headless selenium    
+options=Options() 										#creating options for headless browser session 								#adding arguments for headless browser session  
+options.add_argument("--headless") 
+options.add_argument("--disable-gpu") 
+driver=webdriver.Chrome(executable_path=r"C:\chromedriver\chromedriver",chrome_options=options) 
+driver.get("https://www .youtube .com/upload") 
+driver.find_element_by_id ("text").send_keys("final .mp4") 
+driver.find_element _by _id("upload-prompt-box").click()
