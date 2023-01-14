0205f72a1783756e800fd209f80afff0cf282c0c
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-14 15:01:59 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,52 @@
+ #Python Bot Source
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+ 
+# Set up Chrome options to run headless and disable GPU
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--disable-gpu")  
+ 
+# Create a new instance of the Chrome driver with the options set above 
+driver = webdriver.Chrome(options=chrome_options)  
+
+ # Scrape 10 videos from TikTok 
+for i in range(10): 
+
+    # Open TikTok website in headless browser 
+    driver.get('https://www.tiktok.com/')
+
+    # Find and click on the video element on the page 
+    video = driver.find_element_by_class_name('jsx-3523532890')  
+
+    # Download the video file to local directory 
+    video.click()  
+
+    time.sleep(2)  
+
+ # Concatenate all videos into one file named final.mp4 
+os.system('ffmpeg -f concat -safe 0 -i mylist.txt -c copy final.mp4')  
+
+ # Upload final video to YouTube using headless Selenium browser 
+
+ # Open YouTube website in headless browser 
+driver = webdriver.Chrome(options=chrome_options)  
+
+ # Log into YouTube account using credentials stored in environment variables 
+username = os.environ['YOUTUBE_USERNAME']  
+password = os.environ['YOUTUBE_PASSWORD']  
+
+ # Find and fill out username field on login page with stored credentials 																     driver .findElement(By .name("username")) .sendKeys(username);    
+
+ # Find and fill out password field on login page with stored credentials                                                                       driver .findElement(By .name("password")) .sendKeys(password);    
+
+ # Click submit button to log into account                                                                                                    driver .findElement(By .id("submit-button")) .click();    
+
+ # Navigate to upload page on YouTube website                                                                                                  driver .get('https://www.youtube/upload');    
+
+ # Find and upload final video file from local directory to YouTube account using Selenium WebDriver API methods                         driver .findElement(By .id("upload-prompt-box")) .sendKeys("finalvideofilepath");    
+
+ # Set up loop to repeat process hourly (3600 seconds) while True:      os .system('ffmpeg -f concat -safe 0 -i mylisttxt -c copy finalmp4')      driver = webdriverChrome (options=chromeOptions)      username = os environ['YOUTUBEUSERNAME']      password = os environ['YOUTUBEPASSWORD']      driver findElement (By name("username")) sendKeys (username);      driver findElement (By name("password")) sendKeys (password);      driver findElement (By id("submitbutton")) click();      driver get('https://wwwyoutubecom/upload');      driver findElement (By id("uploadpromptbox")) sendKeys ("finalvideofilepath");       time sleep (3600)
