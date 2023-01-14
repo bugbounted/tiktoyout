4bab17a98e411b789f83f59f03326e422f36b078
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-14 16:36:33 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ options.headless = True
 
 
 # Create a new instance of the Chrome driver 
-browser = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
 
 
 while True:
@@ -29,37 +29,32 @@ while True:
     subprocess.run(["ffmpeg", "-f", "concat", "-safe", "0", "-i", "list.txt", "-c", "copy", "final.mp4"])
 
     # Log in to YouTube using Selenium and upload the concatenated video  
-    browser.get('https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den-GB%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en-GB&ec=65620')
+    driver.get('https://www.youtube.com/upload')
 
-    username_input = browser.find_element_by_name('identifier')  # Find username input field by name attribute  
-    username_input.send_keys(GMAIL_USERNAME)                  # Enter Gmail username into input field  
+ #finding and entering username and password fields  
+username_field = driver.find_element_by_id('identifierId')  #finding username field by id  
+username_field.send_keys(username) #entering username in the field  
 
-    nextButton = browser.find_element_by_id('identifierNext')   # Find Next button by id attribute  												   # Click Next button to proceed to password page  
+ #clicking on next button after entering username  
+nextButton = driver.find_element_by_id('identifierNext')  #finding next button by id  
+nextButton.click()   
 
-    nextButton.click()                                        # Click Next button to proceed to password page  
+ #entering password after clicking on next button   												   		   	   	   	   	       	    	    	    	    
 
-    time.sleep(5)                                             # Wait 5 seconds for page to load before entering password  
+passwordField = driver.find_element_by_name('password') #finding password field by name  
 
-    passwordInput = browser.find_element_by_name('password') # Find password input field by name attribute  
+ #entering password in the field and clicking on next button after entering password     
 
-    passwordInput.send_keys(GMAIL_PASSWORD)               # Enter Gmail password into input field  
+passwordField.send_keys(password)     
 
-    signInButton = browser.find_element_by_id('passwordNext')     # Find Sign In button by id attribute    					       # Click Sign In button to log in to YouTube account    
+ #clicking on next button after entering password     
 
-    signInButton.click()                                     # Click Sign In button to log in to YouTube account    
+nextButton2 = driver.find_element_by_id('passwordNext')     
 
-    time.sleep(5)                                           # Wait 5 seconds for page to load before uploading video    
+nextButton2.click()     
 
-    uploadButton = browser.find_element_by_xpath('//*[@id="upload-prompt-box"]/div[2]/div[1]/span[1]')      # Find Upload Video button by xpath expression    
+ #uploading video file to youtube channel using headless selenium     
 
-    uploadButton.click()                                     # Click Upload Video button    
+uploadButton = driver.find_element_by_xpath("//input[@type='file']")     
 
-    time.sleep(5)                                           # Wait 5 seconds for page to load before selecting file    
-
-    selectFileInput = browser.find_element_by_xpath('//*[@id="start-upload-button-single"]')      # Find Select File input field by xpath expression    
-
-    selectFileInput.send_keys(os._getcwd () + '/final.mp4 ')      # Enter path of concatenated video file into input field    
-
-        publishButton = browser.find_element_by_xpath('//*[@id="upload-item-0"]/div[4]/div[1]/div[1]/span[1]')      # Find Publish Video button by xpath expression    
-
-        publishButton.click()                                   # Click Publish Video button
+uploadButton .sendKeys("final.mp4")
