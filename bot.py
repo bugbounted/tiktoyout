f76f4fcdb8ac7a131c87d4f9c25b6a3b24719a1c
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-14 18:15:51 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
