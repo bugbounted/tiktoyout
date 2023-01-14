ddc84385243223aa79e33bc2022937611e56d850
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-14 18:49:14 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,14 +10,14 @@ import time, os, subprocess
 import asyncio
 from ffmpeg import FFmpeg
 
-path = (chrome)
+path = '/usr/bin/google-chrome'
 service = Service(path)
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 #driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=chrome_options)
 url = "https://www.tiktok.com/"  # Replace with the URL you want to scrape from 
 driver.get(url)
